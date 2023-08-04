e2054d97f29707530d5141f50c9f6cbbacdcc8fe
Rocky Haque <rockyhaque99@gmail.com>
2023-08-04 06:18:06 +0600

    Add files via upload

---
+++ conversion.js
@@ -0,0 +1,19 @@
+var first_name = 'Rocky';
+var last_name = 'Haque';
+
+var full_name = first_name + ' ' + last_name;
+// console.log(full_name);
+
+var num1 = 'jj'
+num2 = parseInt(num1);
+console.log(typeof num2);
+
+var n1 = 0.1;
+var n2 = 0.2;
+
+var sum = n1 + n2;
+
+sum = parseFloat(sum)
+console.log(sum.toFixed(1));
+console.log()
+
