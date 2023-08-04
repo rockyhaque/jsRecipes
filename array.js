5afff0ad2953b7e674487168db0ee625424ac339
Rocky Haque <rockyhaque99@gmail.com>
2023-08-04 06:18:43 +0600

    Add files via upload

---
+++ array.js
@@ -0,0 +1,15 @@
+var names = ['kuki', 'buki', 'cuki'];
+
+// console.log(names);
+// console.log(names.length);
+
+// ele = names[2];
+// console.log(ele);
+
+names[1] = 'Tiki';
+console.log(names);
+
+// * Find Index of An Element
+var positinIndex = names.indexOf('kuki');
+console.log(positinIndex);
+
