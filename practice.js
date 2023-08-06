edc3b2987d210b98b0b20c7a563e888ff79639d0
Rocky Haque <rockyhaque99@gmail.com>
2023-08-06 07:13:09 +0600

    Add files via upload

---
+++ practice.js
@@ -0,0 +1,20 @@
+//* indexOf, pop, push
+
+var fruits = ['Apple', 'Banana', 'Orange'];
+
+var idx = fruits.indexOf('Banana');
+console.log(idx);
+
+fruits[1] = 'Mango';
+console.log(fruits);
+
+fruits.pop();
+console.log(fruits);
+
+fruits.push('Watermelon');
+console.log(fruits);
+
+
+
+
+
