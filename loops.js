1d381a8396363e11ab0633dc2ec028f450d0b41c
Rocky Haque <rockyhaque99@gmail.com>
2023-08-06 07:12:54 +0600

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,14 @@
+var nums = [21, 13, 43, 99, 56, 87, 100];
+
+for(var i = 0; i < nums.length; i++)
+{   
+    var num = nums[i]
+
+    if(num > 50)
+    continue;   // Continue means simply skip
+
+    console.log(num);
+}
+
+
+
