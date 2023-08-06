f5d98167cf424e5ad21fee172bb492d2b2535981
Rocky Haque <rockyhaque99@gmail.com>
2023-08-06 07:12:42 +0600

    Add files via upload

---
+++ largestNumbers.js
@@ -0,0 +1,10 @@
+var a = 13;
+var b = 79;
+var c = 45;
+
+if(a > b && a > c)
+    console.log('A is the largest number')
+else if(b > a && b > c)
+    console.log('B is the largest number')
+else
+    console.log('C is the largest number')
