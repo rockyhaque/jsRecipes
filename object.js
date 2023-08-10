60d76eddd3366bf0cc2da18c618c0b05d1df6a08
Rocky Haque <rockyhaque99@gmail.com>
2023-08-10 15:38:47 +0600

    Add files via upload

---
+++ object.js
@@ -0,0 +1,12 @@
+var myPhone = {
+    brand: 'infinix',
+    camera: '12MP',
+    price: 9990
+}
+
+console.log(myPhone);
+console.log(myPhone.price);
+myPhone.price = 10990;
+console.log(myPhone.price);
+console.log(myPhone);
+
