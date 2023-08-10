2942eaf3f346652515ffd0a471c55f52528a85b4
Rocky Haque <rockyhaque99@gmail.com>
2023-08-10 15:40:58 +0600

    Add files via upload

---
+++ factorialReverse.js
@@ -0,0 +1,29 @@
+// * Factorial using for loop
+// function factorial(number){
+//     let result = 1;
+//     for(let i = number; i >= 1; i--){
+//         result *= i;
+//         console.log(i);
+//     }
+//     return result;
+// }
+
+// const givenNumber = 9;
+// const fact = factorial(givenNumber);
+// console.log(fact);
+
+
+//* Factorial using while loop
+// function factorial(number){
+//     let result = 1;
+//     let i = number;
+//     while(i >= 1){
+//         result *= i;
+//         i--;
+//     }
+//     return result;
+// }
+
+// const givenNumber = 8;
+// const fact = factorial(givenNumber);
+// console.log(fact);
