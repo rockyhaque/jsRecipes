f882330ac6e4e0f63740cb5b3d498ec8c8969aec
Rocky Haque <rockyhaque99@gmail.com>
2023-08-10 15:40:17 +0600

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,11 @@
+const color = 'yellow';
+
+switch(color){
+    case 'green':
+        console.log('You are a green friend');
+        break;
+    case 'red':
+        console.log('You are a red friend');
+    default:
+        console.log('I got you! Yellow');
+}
