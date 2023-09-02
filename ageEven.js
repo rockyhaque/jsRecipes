f245b937064226864ff7b5a2a7f75309e4b62a52
Rocky Haque <rockyhaque99@gmail.com>
2023-09-02 18:52:03 +0600

    Add files via upload

---
+++ ageEven.js
@@ -0,0 +1,13 @@
+function ageEven(friendsAge){
+    for(let i = 0; i < friendsAge.length; i++){
+        const item = friendsAge[i]
+        let evens = item % 2;
+        if(evens == 0){
+            console.log(friendsAge[i]);
+        }
+    }
+    return false;
+}
+
+const ages = [22, 23, 44];
+console.log(ageEven(ages));
