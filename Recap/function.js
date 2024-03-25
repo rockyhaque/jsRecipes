ba2b52abe774465201062cda50d5c2070c7d260e
Rocky Haque <rockyhaque99@gmail.com>
2024-03-25 11:32:24 +0600

    Create function.js

---
+++ Recap/function.js
@@ -0,0 +1,53 @@
+//*** Task 1 - Take a number if the number is odd multiply it by 2 and return the result. If the number is even divide it by two and return the result.
+
+
+function evenOdd(num){
+    if(num % 2 !== 0){
+        return num * 2;
+    } else{
+        return num / 2;
+    }
+}
+
+// console.log(evenOdd(11));
+
+
+
+//* Task 2 - Write a function called make_avg() which will take an array of integers and the size of that array and return the average of those values.
+function make_avg(arr, n){
+    let sum = 0, avg;
+    for(let i = 0; i < n; i++){
+        sum += arr[i]
+        avg = sum / n;
+    }
+    return avg;
+}
+
+const inputArray = [1, 2, 3, 4, 5, 6];
+// console.log(make_avg(inputArray, 6));
+
+
+//* Task 3 - Write a function called count_zero() which will take a binary string (Binary string is a string which is consist of only 0 and 1) as parameter and count how many 0’s are there in that string.
+
+function count_zero(str){
+    let zeroCount = 0;
+    for(let digit of str){
+        
+        if(digit == !'0' || digit == !'1'){
+            console.log("Wrong input");
+            // return 0;
+            // return "Wrong input";
+            // break; 
+        } 
+
+        if(digit === '0'){
+            zeroCount++
+        }
+         
+    }
+    return zeroCount;
+
+}
+
+console.log(count_zero("0001110101"));
+
