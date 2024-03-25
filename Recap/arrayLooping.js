67d6e7ad2d3d0406ec2179a9dfc3a5ed01d28488
Rocky Haque <rockyhaque99@gmail.com>
2024-03-25 12:28:12 +0600

    Update arrayLooping.js

--- Recap/arrayLooping.js
+++ Recap/arrayLooping.js
@@ -124,3 +124,19 @@ for(let height2 of heights2){
 }
 
 // console.log(minHeights2);
+
+
+
+//* Task 7 - Find the friend with the smallest name.
+
+const names = ['rahim', 'robin', 'rafi', 'ron', 'rashed'];
+let smallestName = names[0];
+
+for(let i = 0; i < names.length; i++){
+    
+    if(names[i].length < smallestName.length){
+        smallestName = names[i];
+    }
+}
+
+console.log(smallestName);
